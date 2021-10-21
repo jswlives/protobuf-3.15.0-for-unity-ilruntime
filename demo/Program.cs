84a3acec3edc48ee5d744a93106396b8af73173d
jiangshaowei <jiangshaowei@gamesci.com.cn>
2021-10-21 13:15:08 +0800

    add demo & release

---
+++ demo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Demo;
+using System.Diagnostics;
+using Google.Protobuf;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person
+            {
+                SchoolAddr = new Address()
+            };
+            //Debug.Assert(!person.HasId2);
+
+            byte[] result = new byte[person.CalculateSize()];
+            CodedOutputStream output = new CodedOutputStream(result);
+            person.WriteTo(output);
+
+
+            Person person1 = new Person();
+            person1.MergeFrom(result);
+
+            Console.WriteLine(person1.Id2);
+
+            if(person1.SchoolAddr != null)
+            {
+                Console.WriteLine("has school!");
+            }
+
+            Console.WriteLine(person1.Id2);
+            if(person1.HasId2)
+            {
+                Console.WriteLine("has id2!");
+            }
+            Console.WriteLine("enter key!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ demo/proto/build_proto.bat
@@ -0,0 +1,1 @@
+.\protoc.exe .\demo.proto --csharp_out=.
